a50a525362f52cec4d65326c445440e2eac2ee32
melquiadesmario <melquiadesm@gmail.com>
2020-10-19 13:14:48 -0300

    Handling Exceptions

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import path from 'path';
+import 'express-async-errors';
 
 import './database/connection';
 
 import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
 
@@ -12,5 +14,6 @@ const port = process.env.PORT || 3333;
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${ port }`));
